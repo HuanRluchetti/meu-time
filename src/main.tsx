8cf013195bf51368685709fe2a3abc2edd560d39
Huan <huan.luchetti@gmail.com>
2023-05-18 00:38:26 -0300

    firsts routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+// Páginas
+import {Home, NewPost} from "./routes";
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>,
+      },
+      {
+        path: "/new",
+        element:<NewPost/>,
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {InitScreen} from "./../screens"
+
+const Home = () => {
+  return (
+    <InitScreen/>
+  )
+}
+
+export default Home;
